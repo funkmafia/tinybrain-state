cbb8e2ba5532ad591a0ca004faf4bac2a7aa1dfa
Zak <zak.hajjaji@icloud.com>
2025-10-22 22:06:06 +0100

    Feat: New slector function inside createStore so listener only runs when their slice changes, and mirros how react components useSelector-style patterns.

--- packages/state-core/store.ts
+++ packages/state-core/store.ts
@@ -1,10 +1,11 @@
 export type Listener<T> = (next: T, prev: T) => void; 
-
 // export = makes this type available to other files 
 // type = define a new type alias 
 // <T> = generic placeholder (works with any state type) 
 // (next: T, prev: T) = function that takes 2 arguments: the new state and the previous state. 
 // purpose = to describe subscribers that react to state changes. 
+export type Selector<T, S> = (state: T) => S; // selector function that takes state and returns a value of type S. 
+export type EqualityFn<S> = (a: S, b: S) => boolean; // true if equal, false if not. 
 
 export interface Store<T> {
     get: () => T;
@@ -12,8 +13,11 @@ export interface Store<T> {
     subscribe: (listener: Listener<T>) => () => void; 
     replace: (next: T) => void;
     reset: () => void;  
+    select?: <S> (
+        selector: Selector<T, S>,
+        eq?: EqualityFn<S>
+    ) => (listener: (next: S, prev: S) => void) => () => void; 
 }
-
 // store<T> = generic container for any type of state 
 // get = get the current state 
 // set = update the state 
@@ -21,7 +25,6 @@ export interface Store<T> {
 // T = for example { name: string; age: number }
 // return value = a function that unsubscribes the listener when called. 
 // why = lets us update safely with strong typing 
-
 export function createStore<T>(initial: T): Store<T> {
    let state = initial; 
    const listeners = new Set<Listener<T>>(); 
@@ -42,6 +45,20 @@ export function createStore<T>(initial: T): Store<T> {
     return () => listeners.delete(fn); 
    }; 
 
-   return { get, set, subscribe, replace, reset }; 
+   const select = <S,> (selector: Selector<T, S>, eq?: EqualityFn<S>) => {
+    let prevSlice = selector(state); 
+    return (listener: (next: S, prev: S) => void) => {
+        return subscribe((nextState) => {
+            const nextSlice = selector(nextState);
+            if (!eq || !eq(prevSlice, nextSlice)) {
+                const prev = prevSlice;
+                prevSlice = nextSlice;
+                listener(nextSlice, prev);
+            }
+        });
+    };
+        };
+
+   return { get, set, subscribe, replace, reset, select}; 
 }
 
